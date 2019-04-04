6c06331ae472a07ba8d2538829c4d78ef0b72133
jhyates <jhyates@princeton.edu>
2019-04-04 16:16:48 -0400

    Initial IMU Preintegration commit First commit from Joe's machine (Win10), set up framework for working on IMU preintegration.

---
+++ imu_preint_matlab/imu_preint_test.m
@@ -0,0 +1,10 @@
+% NAVARCH 568 W19 Group 10
+% Chen, Dai, Lu, Yates
+% Final Project: IMU Preintegration
+%
+% Script: Run this in its current directory using MATLAB.
+%
+% Contributors: Joseph Yates
+% Last Edited: 20190404
+
+disp('TBD');
