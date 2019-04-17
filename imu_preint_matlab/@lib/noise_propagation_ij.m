d1494bafa29be11b1dc06c9454a053cb99927147
jhyates <jhyates@umich.edu>
2019-04-16 23:59:48 -0400

    Added lifted jacobians and added some comments to noise_propagation_ij.m.

--- imu_preint_matlab/@lib/noise_propagation_ij.m
+++ imu_preint_matlab/@lib/noise_propagation_ij.m
@@ -1,32 +1,44 @@
 function Sigma = noise_propagation_ij(t,omega,accel,Sigma)
-dt = diff(t);               % 1 by 20
-theta = zeros(3,length(dt));% 3 by 20
-theta_init = zeros(3,1);    % 3 by 1
-for k = 1:legnth(dt)
-    theta(:,k) = theta_init+jacobian(theta_init)^-1*omega(:,k)*dt(k);
+    % NAVARCH 568 W19 Group 10
+    % Chen, Dai, Lu, Yates
+    % Final Project: IMU Preintegration
+    %
+    % Function: call with arguments.
+    %
+    % Contributors: Yizhou Lu, Joseph Yates
+    % Last Edited: 20190416
+
+    dt = diff(t);                % 1 by 20
+    theta = zeros(3,length(dt)); % 3 by 20
+    theta_init = zeros(3,1);     % 3 by 1
     
-    Sigma_g = 0.0035*2*pi/360*sqrt(2000)*eye(3);
-    Sigma_a = 0.14e-3*9.80665*sqrt(2000)*eye(3);
-    
-    Rk = expm(R3_to_so3(theta(:,k)));
-    
-    Ak = eye(9);
-    Ak(1:3,1:3) = eye(3)-dt(k)/2*R3_to_so3(omega(:,k));
-    Ak(4:6,1:3) = 1/2*Rk*R3_to_so3(-accel(:,k))*jacobian(theta(:,k))*dt(k)^2;
-    Ak(4:6,7:9) = eye(3)*dt(k);
-    Ak(7:9,1:3) = Rk*R3_to_so3(-accel(:,k))*jacobian(theta(:,k))*dt(k);
-    
-    Bk = zeros(9,3);
-    Bk(4:6,:) = 1/2*Rk*dt(k)^2;
-    Bk(7:9,:) = Rk*dt(k);
-    
-    Ck = zeros(9,3);
-    Ck(1:3,:) = jacobian(theta(:,k))^-1*dt(k);
-    
-    Sigma = Ak*Sigma*Ak'+Bk*Sigma_a*Bk'+Ck*Sigma_g*Ck';
-    
-    theta_init = theta(:,k);
-end
+    for k = 1:length(dt)
+        theta(:,k) = theta_init+jacobian(theta_init)^-1*omega(:,k)*dt(k);
+
+        Sigma_g = 0.0035*2*pi/360*sqrt(2000)*eye(3);
+        Sigma_a = 0.14e-3*9.80665*sqrt(2000)*eye(3);
+
+        Rk = expm(R3_to_so3(theta(:,k)));
+
+        Ak = eye(9);
+        Ak(1:3,1:3) = eye(3)-dt(k)/2*R3_to_so3(omega(:,k));
+        Ak(4:6,1:3) = 1/2*Rk*vector_to_so3(-accel(:,k))* ...
+            jacobian(theta(:,k))*dt(k)^2;
+        Ak(4:6,7:9) = eye(3)*dt(k);
+        Ak(7:9,1:3) = Rk*vector_to_so3(-accel(:,k))* ...
+            jacobian(theta(:,k))*dt(k);
+
+        Bk = zeros(9,3);
+        Bk(4:6,:) = 1/2*Rk*dt(k)^2;
+        Bk(7:9,:) = Rk*dt(k);
+
+        Ck = zeros(9,3);
+        Ck(1:3,:) = jacobian(theta(:,k))^-1*dt(k);
+
+        Sigma = Ak*Sigma*Ak'+Bk*Sigma_a*Bk'+Ck*Sigma_g*Ck';
+
+        theta_init = theta(:,k);
+    end
 end
 
 % function H = jacobian(theta)
---
+++ imu_preint_matlab/@lib/preintegration_factor.m
@@ -0,0 +1,98 @@
+function [r_ij, J_ij] = preintegration_factor(R_i, R_j, v_i, v_j, p_i, ...
+    p_j, dR_ij, dv_ij, dp_ij, dt_ij, g)
+    % NAVARCH 568 W19 Group 10
+    % Chen, Dai, Lu, Yates
+    % Final Project: IMU Preintegration
+    %
+    % Function: call with arguments. Generates the residual and the
+    % jacobianfor the IMU factor based on the relative motion between
+    % keyframes i and j. This relies on the preintegrated IMU measurement,
+    % as well as estimates of the vehicle postion, velocity, and
+    % orientation at i and j. These estimates may be ground truth in the
+    % IMU demo case, or decision variables when the IMU factor is used with
+    % Graph SLAM. This corresponds to equation (45) and Appendix IX-C
+    % (Equations (70) - (81)) in Forster, et al. (2016).
+    %
+    % Inputs:
+    %   R_i         Rotation matrix estimate of body frame w.r.t. world
+    %               frame at current timestep i, SO(3)
+    %   R_j         Rotation matrix estimate of body frame w.r.t. world
+    %               frame at current timestep j, SO(3)
+    %   v_i         World-frame velocity estimate at i, R^3 
+    %   v_j         World-frame velocity estimate at j, R^3 
+    %   p_i         World-frame position estimate at i, R^3 
+    %   p_j         World-frame position estimate at j, R^3 
+    %   dR_ij       Relative rotation matrix estimate of body frame w.r.t.
+    %               world frame at j (relative to i), SO(3)
+    %   dv_ij       Relative world-frame velocity estimate at j (relative
+    %               to i), R^3 
+    %   dp_ij       Relative world-frame position estimate at j (relative
+    %               to i), R^3 
+    %   dt_ij       Time intervals at timesteps between i and j,
+    %               according to IMU timekeeping, each R^1
+    %   g           World-frame gravitational acceleration, [0 0 -9.81]
+    %
+    % Intermediate quantities:
+    %   r_dRij      Relative rotation residual for an IMU factor between
+    %               keyframes i and j, R^3
+    %   r_dvij     	Relative velocity residual for an IMU factor between
+    %               keyframes i and j, R^3
+    %   r_dpij     	Relative position residual for an IMU factor between                                                                                           
+    %               keyframes i and j, R^3
+    %   J_dRij      Jacobian of r_dRij
+    %   J_dvij      Jacobian of r_dvij
+    %   J_dpij      Jacobian of r_dpij
+    %
+    % Outputs:
+    %   r_ij        Stacked state residual for this factor
+    %   J_ij        Stacked residual error Jacobian for this factor
+    %
+    % Contributors: Joseph Yates
+    % Last Edited: 20190416
+    
+    % Sum total time increment (IMU timekeeping)
+    sdt_ij = sum(dt_ij);
+    
+    % Equation (45) - residual calculation
+    r_dRij = log_map((dR_ij)' * R_i' * R_j);
+    
+    r_dvij = R_i' * (v_j - v_i - g*sdt_ij) - (dv_ij);
+    
+    r_dpij = R_i' * (p_j - p_i - v_i*sdt_ij - 0.5*g*sdt_ij^2) - ...
+        (dp_ij);
+    
+    % Because of the rotation, the nonlinear solver uses a
+    % Lift-Solve-Retract technique. These Jacobians are defined such that
+    % the cost function has been lifted; retractions are already
+    % substituted. See Equation (70) in Forster, et al. (2016).
+    
+    % d-vector formation: dphi_i, dv_i, dp_i, dphi_j, dv_j, dp_j
+    
+    % Jacobian of r_dpij (R^3x18):
+    J_dpij = [vector_to_so3(R_i'*(p_j-p_i-v_i*sdt_ij-0.5*g*sdt_ij^2)) ...
+              -R_i'*sdt_ij ... % dv_i                           % dphi_i
+              -eye(3,3) ...    % dp_i
+              zeros(3,3) ...   % dphi_j
+              zeros(3,3) ...   % dv_j
+              R_i'*R_j];       % dp_j
+    
+    % Jacobian of r_dvij (R^3x18):
+    J_dvij = [vector_to_so3(R_i'*(v_j-v_i-g*sdt_ij)) ... % dphi_i
+              -R_i' ...      % dv_i
+              zeros(3,3) ... % dp_i
+              zeros(3,3) ... % dphi_j
+              R_i' ...       % dv_j
+              zeros(3,3)];   % dp_j
+      
+    % Jacobian of r_dRij (R^3x18), Equation (81):
+    J_dRij = [-inv_right_jacobian_map(r_dRij)*R_j'*R_i ... % dphi_i
+              zeros(3,3) ... % dv_i
+              zeros(3,3) ... % dp_i
+              inv_right_jacobian_map(r_dRij) ... % dphi_j
+              zeros(3,3) ... % dv_j
+              zeros(3,3)];   % dp_j
+          
+    % Stack quantities
+    r_ij = [r_dRij; r_dvij; r_dpij];
+    J_ij = [J_dRij; J_dvij; J_dpij];
+end
