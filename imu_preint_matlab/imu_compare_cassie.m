735304ce4876e51af5956d422a0a7ff9d3695869
jhyates <jhyates@umich.edu>
2019-04-18 15:48:55 -0400

    Working version of cassie compare. Still needs to either handle bug for spreading apart the keyframes or needs more efficient plotting code.

--- imu_preint_matlab/imu_compare_cassie.m
+++ imu_preint_matlab/imu_compare_cassie.m
@@ -10,7 +10,7 @@
 % MATLAB toolbox.
 %
 % Contributors: Joseph Yates
-% Last Edited: 20190417
+% Last Edited: 20190418
 
 %% Setup
 disp('-- Setting up environment');
@@ -45,7 +45,7 @@ gt.v = approx_velocity_gt(mocap.time, mocap.position);
 % We have more IMU data than MoCap data, so using MoCap as the
 % "keyframes" should work fine. For this proof-of-concept test, we'll use
 % every 10th GT data point time as the keyframe time. 
-gt_inc = 10;
+gt_inc = 1;
 gt.i = 1:gt_inc:length(mocap.time)-1;
 n_kf = length(gt.i); % number of keyframes
 gt.dt = 0.01; % approximated/hard-coded average time between keyframes
@@ -56,8 +56,13 @@ plot3(gt.p(1,:),gt.p(2,:),gt.p(3,:),'-b');
 axis equal
 hold on;
 
+figure(2)
+plot(gt.p(1,:),gt.p(2,:),'-b');
+axis equal
+hold on;
 
-%% GTSAM Implementation
+
+%% GTSAM Implementation ("Baseline")
 disp('-- Setting up GTSAM solution')
 
 % Interpret metadata and compute relative sensor pose transforms
@@ -91,25 +96,34 @@ currentBias = imuBias.ConstantBias(zeros(3,1), zeros(3,1));
 % Consider changing this noise model to Gaussian
 sigma_init_x = noiseModel.Isotropic.Precisions([0.0; 0.0; 0.0; 1; 1; 1]);
 sigma_init_v = noiseModel.Isotropic.Sigma(3, 1000.0);
-sigma_init_b = noiseModel.Isotropic.Sigmas([0.100; 0.100; 0.100; 5.00e-05; 5.00e-05; 5.00e-05]);
+sigma_init_b = noiseModel.Isotropic.Sigmas([0.100; 0.100; 0.100; ...
+    5.00e-05; 5.00e-05; 5.00e-05]);
 sigma_between_b = [IMU_metadata.AccelerometerBiasSigma * ones(3,1); ...
     IMU_metadata.GyroscopeBiasSigma * ones(3,1)];
 w_coriolis = [0;0;0];
 
 % Solver object
 isamParams = ISAM2Params;
-isamParams.setFactorization('CHOLESKY');
+isamParams.setFactorization('QR');
 isamParams.setRelinearizeSkip(10);
 isam = gtsam.ISAM2(isamParams);
 newFactors = NonlinearFactorGraph;
 newValues = Values;
+
+% Data collection (in a not-GTSAM format)
+bl.t = zeros(1, length(gt.i));
+bl.p = zeros(3, length(gt.i));
+bl.v = zeros(3, length(gt.i));
+bl.ba = zeros(3, length(gt.i));
+bl.bg = zeros(3, length(gt.i));
+bl.R = {};
 
 % Solution loop
 disp('-- Starting GTSAM solution loop')
 IMUtimes = [IMU_data.Time];
 
 for ii = gt.i
-  
+  disp(['Iteration: ' num2str(ii)]);
   % At each "keyframe" we initialize a new node in the graph (use truth
   % data "keyframe" time
   currentPoseKey = symbol('x',ii);
@@ -128,9 +142,18 @@ for ii = gt.i
         currentVelocityGlobal, sigma_init_v));
     newFactors.add(PriorFactorConstantBias(currentBiasKey, currentBias, ...
         sigma_init_b));
+    
+    % Store data
+    bl.t(1,ii) = t;
+    bl.R(1,ii) = {currentPoseGlobal.rotation().matrix()};
+    bl.p(:,ii) = currentPoseGlobal.translation().vector();
+%     bl.v(:,ii) = currentVelocityGlobal.vector();
+%     bothbias = currentBias.vector();
+%     bl.ba(:,ii) = bothbias(1:3);
+%     bl.bg(:,ii) = bothbias(4:6);
   else
     % Summarize IMU data between the previous and current "keyframe"
-    t_previous = gt.t(ii-1);
+    t_previous = gt.t(ii-gt_inc);
     IMUindices = find(IMUtimes >= t_previous & IMUtimes <= t);
     
     currentSummarizedMeasurement = ...
@@ -151,12 +174,12 @@ for ii = gt.i
     
     % Create IMU factor
     newFactors.add(ImuFactor( ...
-        currentPoseKey-10, currentVelKey-10, currentPoseKey, ...
+        currentPoseKey-gt_inc, currentVelKey-gt_inc, currentPoseKey, ...
         currentVelKey, currentBiasKey, currentSummarizedMeasurement, ...
         g, w_coriolis));
     
     % Bias evolution as given in the IMU metadata
-    newFactors.add(BetweenFactorConstantBias(currentBiasKey-10, ...
+    newFactors.add(BetweenFactorConstantBias(currentBiasKey-gt_inc, ...
         currentBiasKey, imuBias.ConstantBias(zeros(3,1), zeros(3,1)), ...
         noiseModel.Diagonal.Sigmas(sqrt(numel(IMUindices)) * ...
         sigma_between_b)));
@@ -170,29 +193,55 @@ for ii = gt.i
     % =====================================================================
     % We accumulate 3 total keyframes before updating the solver
     % at first so that the heading becomes observable.
-    if ii > 10 % firstGPSPose + 2*GPSskip
+%     if ii > 10 % firstGPSPose + 2*GPSskip
         isam.update(newFactors, newValues);
         newFactors = NonlinearFactorGraph;
         newValues = Values;
       
-        if rem(ii,10)==0 % plot every 10 "keyframes"
-            cla;
+%         if rem(ii,100)==1 % plot every 10 "keyframes"
+          if ii == gt.i(end)
+%             cla;
+            figure(1)
+            hold on;
             plot3DTrajectory(isam.calculateEstimate, 'g-');
-            title('Estimated trajectory using ISAM2 (IMU+GPS)')
-            xlabel('[m]')
-            ylabel('[m]')
-            zlabel('[m]')
+%             title('Estimated trajectory using ISAM2 (IMU+GPS)')
+%             xlabel('[m]')
+%             ylabel('[m]')
+%             zlabel('[m]')
             axis equal
             drawnow;
-        end
+            
+            figure(2)
+            hold on;
+            plot2DTrajectory(isam.calculateEstimate,'g-')
+            axis equal
+            drawnow;
+          end
         % =================================================================
         currentPoseGlobal = isam.calculateEstimate(currentPoseKey);
         currentVelocityGlobal = isam.calculateEstimate(currentVelKey);
         currentBias = isam.calculateEstimate(currentBiasKey);
-    end
+        
+        % Store data
+        bl.t(1,ii) = t;
+        bl.R(1,ii) = {currentPoseGlobal.rotation().matrix()};
+        bl.p(:,ii) = currentPoseGlobal.translation().vector();
+%         bl.v(:,ii) = currentVelocityGlobal.vector();
+%         bothbias = currentBias.vector();
+%         bl.ba(:,ii) = bothbias(1:3);
+%         bl.bg(:,ii) = bothbias(4:6);
+%     end
   end
 end % end main loop
 
+% Plot results
+% figure(1)
+% plot3(bl.p(1,:),bl.p(2,:),bl.p(3,:),'-g');
+% hold on;
+% 
+% figure(2)
+% plot(bl.p(1,:),bl.p(2,:),'-g');
+% hold on;
 
 %% Our implementation
 
