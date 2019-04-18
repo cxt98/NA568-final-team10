01d67007fa95c7e44d901e73e8e66ebb1abde095
DeyangDai <deyangd@umich.edu>
2019-04-17 20:49:51 -0400

    Added modified ORB-SLAM2 with IMU preintegration for stereo (not stable)

---
+++ ORB_SLAM2/Examples/Stereo/stereo_kitti_imu.cc
@@ -0,0 +1,224 @@
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<iomanip>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
+#include<System.h>
+
+using namespace std;
+
+void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
+                vector<string> &vstrImageRight, vector<double> &vTimestamps);
+
+void LoadIMUData(const string &strPathToIMU, vector<ORB_SLAM2::IMUData> &vimuData);
+
+int main(int argc, char **argv)
+{
+    if(argc != 5)
+    {
+        cerr << endl << "Usage: ./stereo_kitti_imu path_to_vocabulary path_to_settings path_to_sequence "
+             << "path_to_imu_data(XXX/10Hz_sync_IMU_GPS)" << endl;
+        return 1;
+    }
+
+    // Retrieve paths to images
+    vector<string> vstrImageLeft;
+    vector<string> vstrImageRight;
+    vector<double> vTimestamps;
+    LoadImages(string(argv[3]), vstrImageLeft, vstrImageRight, vTimestamps);
+
+    const int nImages = vstrImageLeft.size();
+
+    // Load IMU data
+    vector<ORB_SLAM2::IMUData> vimuData;
+    LoadIMUData(string(argv[4]), vimuData);
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO,true);
+
+    ORB_SLAM2::ConfigParam config(argv[2]);
+
+    // Vector for tracking time statistics
+    vector<float> vTimesTrack;
+    vTimesTrack.resize(nImages);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+    cout << "Images in the sequence: " << nImages << endl << endl;   
+
+    // Main loop
+    cv::Mat imLeft, imRight;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        // Read left and right images from file
+        imLeft = cv::imread(vstrImageLeft[ni],CV_LOAD_IMAGE_UNCHANGED);
+        imRight = cv::imread(vstrImageRight[ni],CV_LOAD_IMAGE_UNCHANGED);
+        double tframe = vTimestamps[ni];
+
+        if(imLeft.empty())
+        {
+            cerr << endl << "Failed to load image at: "
+                 << string(vstrImageLeft[ni]) << endl;
+            return 1;
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Pass the images to the SLAM system
+        vector<ORB_SLAM2::IMUData> vimuData_one_frame{vimuData[ni]};
+        SLAM.TrackStereoVIO(imLeft, imRight, vimuData_one_frame, tframe);
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        vTimesTrack[ni]=ttrack;
+
+        // Wait to load the next frame
+        double T=0;
+        if(ni<nImages-1)
+            T = vTimestamps[ni+1]-tframe;
+        else if(ni>0)
+            T = tframe-vTimestamps[ni-1];
+
+        if(ttrack<T)
+            usleep((T-ttrack)*1e6);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Tracking time statistics
+    sort(vTimesTrack.begin(),vTimesTrack.end());
+    float totaltime = 0;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        totaltime+=vTimesTrack[ni];
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
+    cout << "mean tracking time: " << totaltime/nImages << endl;
+
+    // Save camera trajectory
+    SLAM.SaveTrajectoryKITTI("CameraTrajectory.txt");
+
+    return 0;
+}
+
+void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
+                vector<string> &vstrImageRight, vector<double> &vTimestamps)
+{
+    ifstream fTimes;
+    string strPathTimeFile = strPathToSequence + "/times.txt";
+    fTimes.open(strPathTimeFile.c_str());
+    while(!fTimes.eof())
+    {
+        string s;
+        getline(fTimes,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            ss >> t;
+            vTimestamps.push_back(t);
+        }
+    }
+
+    string strPrefixLeft = strPathToSequence + "/image_0/";
+    string strPrefixRight = strPathToSequence + "/image_1/";
+
+    const int nTimes = vTimestamps.size();
+    vstrImageLeft.resize(nTimes);
+    vstrImageRight.resize(nTimes);
+
+    for(int i=0; i<nTimes; i++)
+    {
+        stringstream ss;
+        ss << setfill('0') << setw(6) << i;
+        vstrImageLeft[i] = strPrefixLeft + ss.str() + ".png";
+        vstrImageRight[i] = strPrefixRight + ss.str() + ".png";
+    }
+}
+
+void LoadIMUData(const string &strPathToIMU, vector<ORB_SLAM2::IMUData> &vimuData)
+{
+    vector<double> vTimestamps;
+    ifstream file_time;
+    string strPathTimeFile = strPathToIMU + "/times_imu.txt";
+    file_time.open(strPathTimeFile.c_str());
+    while(!file_time.eof())
+    {
+        string s;
+        getline(file_time,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            ss >> t;
+            vTimestamps.push_back(t);
+        }
+    }
+
+    string strPrefix = strPathToIMU + "/data/";
+
+    const int nTimes = vTimestamps.size();
+
+    for(int i=0; i<nTimes; i++)
+    {
+        stringstream ss;
+        ss << setfill('0') << setw(10) << i;
+        string imu_file_path = strPrefix + ss.str() + ".txt";
+        ifstream file_imu;
+        file_imu.open(imu_file_path.c_str());
+
+        double gx, gy, gz; // angular velocity
+        double ax, ay, az; // acceleration
+        double temp;
+        for (int j = 0; j < 11; j++)
+        {
+            file_imu >> temp;
+        }
+        file_imu >> ax >> ay >> az >> temp >> temp >> temp >> gx >> gy >> gz;
+
+        ORB_SLAM2::IMUData imuData{gx, gy, gz, ax, ay, az, vTimestamps[i]};
+        vimuData.push_back(imuData);
+        file_imu.close();
+
+//        cout << imuData;
+//        cin.get();
+    }
+}
---
+++ ORB_SLAM2/run_stereo_imu.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./Examples/Stereo/stereo_kitti_imu Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /home/deyangd/Downloads/KITTI/dataset/sequences/00 /home/deyangd/Downloads/KITTI/10Hz_sync_IMU_GPS
